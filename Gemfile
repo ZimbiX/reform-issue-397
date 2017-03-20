fa46dad1b3865a559c40f12e1cbf7890146ec2d2
Brendan Weibrecht <brendan@weibrecht.net.au>
2017-03-20 17:43:33 +1100

    dry-validation finally requires without failing with: /home/brendan/.rbenv/versions/2.3.1/lib/ruby/gems/2.3.0/gems/dry-validation-0.9.5/lib/dry/validation.rb:11:in `require': cannot load such file -- dry/logic/predicate (LoadError)
    Run using: be ruby test.rb

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+ruby '2.3.1'
+
+gem 'thor'
+
+gem 'trailblazer'
+
+gem 'require_all'
+
+gem 'reform'
+gem 'dry-validation'
+
+gem 'rest-client'
+gem 'nokogiri'
+
+gem 'trailblazer-cells'
+gem 'cells-slim'
+gem 'sass'
+
+group :development do
+  # Pry goodies
+  gem 'jazz_fingers'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'capybara'
+end
---
+++ test.rb
@@ -0,0 +1,51 @@
+puts '0'
+require 'dry-validation'
+puts '1'
+require 'bundler/setup'
+puts '2'
+Bundler.require :default
+puts '3'
+# require 'rspec'
+# require 'dry/logic'
+# require 'dry/logic/predicates'
+# require 'dry-validation'
+# require 'reform'
+# require 'reform/form/dry'
+# Reform::Form.send :feature, Reform::Form::Dry
+
+# class Direction
+# end
+
+# RSpec.describe "type?" do
+#   let(:schema) do
+#     Dry::Validation.Schema do
+#       required(:facing).value(type?: Direction)
+#     end
+#   end
+
+#   class Form < Reform::Form
+#     # ::Dry::Types.register_class Direction
+#     property :facing, virtual: true
+#     validation do
+#       required(:facing).filled(type?: Direction)
+#     end
+#   end
+
+#   let(:input_valid)   { {facing: Direction.new} }
+#   let(:input_invalid) { {facing: Object.new} }
+
+#   it "as regular ruby" do
+#     expect(input_valid[:facing].class).to eq Direction
+#     expect(input_invalid[:facing].class).to_not eq Direction
+#   end
+
+#   it "with dry-validation" do
+#     expect(schema.call(input_valid).success?).to eq true
+#     expect(schema.call(input_invalid).success?).to eq false
+#   end
+
+#   it "with reform" do
+#     expect(Form.new(0).validate(input_valid)).to eq true
+#     expect(Form.new(0).validate(input_invalid)).to eq false
+#   end
+# end
